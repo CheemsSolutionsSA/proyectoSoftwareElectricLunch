e9e4c859dcd4f56a0686b795e3485907c608498f
HectorRubio2 <66961920+hector26rubio2@users.noreply.github.com>
2021-12-12 19:04:11 -0500

    oee

--- resources/views/dashboard/restaurant/index.blade.php
+++ resources/views/dashboard/restaurant/index.blade.php
@@ -1,6 +1,6 @@
 @extends('dashboard.master')
 @section('content')
-    <a href="{{ route('restaurant.create')}}" class="btn btn-info btn-small mb-3">Create Restaurant</a>
+    <a href="{{ route('restaurant.create') }}" class="btn btn-info btn-small mb-3">Create Restaurant</a>
     <h6>List Restaurants</h6>
     <table id="example" class="table table-striped table-bordered" style="width:100%">
         <thead>
@@ -11,98 +11,73 @@
                 <th>Telefono</th>
                 <th>Email</th>
                 <th>Capacidad</th>
+                <th></th>
             </tr>
         </thead>
         <tbody>
             @foreach ($restaurants as $restaurant)
-            <tr>
-                <td scope="row">{{ $restaurant -> id}}</td>
-                <td>{{ $restaurant -> NIT}}</td>
-                <td>{{ $restaurant -> name}}</td>
-                <td>{{ $restaurant -> address}}</td>
-                <td>{{ $restaurant -> phone_number}}</td>
-                <td>{{ $restaurant -> email}}</td>
-                <td>{{ $restaurant -> capacity}}</td>
-                <td><a href="{{ route('restaurant.edit', $restaurant -> id)}}" class="btn btn-info">Editar</a></td>
-                <td><a href="{{ route('restaurant.show', $restaurant -> id)}}" class="btn btn-info">Ver</a></td>
-                <td><button type="button" class="btn btn-danger" onload="eliminar" data-id="{{ $restaurant->id }}" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                    Eliminar
-                </button></td>
-            </tr>
+                <tr>
+
+                    <td>{{ $restaurant->NIT }}</td>
+                    <td>{{ $restaurant->name }}</td>
+                    <td>{{ $restaurant->address }}</td>
+                    <td>{{ $restaurant->phone_number }}</td>
+                    <td>{{ $restaurant->email }}</td>
+                    <td>{{ $restaurant->capacity }}</td>
+                    <td><a href="{{ route('restaurant.edit', $restaurant->id) }}" class="btn btn-info">Editar</a></td>
+                    <td><a href="{{ route('restaurant.show', $restaurant->id) }}" class="btn btn-info">Ver</a></td>
+                    <td><button data-toggle="modal" class="btn btn-danger btn-sm" data-bs-toggle="modal"
+                            data-target="#exampleModal" data-id="{{ $restaurant->id }}">Eliminar</button></td>
+                </tr>
             @endforeach
-            
-        </tfoot>
+
+            </tfoot>
     </table>
 @endsection
 
-{{ $restaurants->links()}}
+{{ $restaurants->links() }}
 
-{{-- <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel"></h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
-                ¿Seguro deseas eliminar la publicación?
+                ¿Seguro deseas eliminar el restaurante?
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
-                <form id="delete" action="{{ route('restaurant.destroy',0) }}" method="POST"
-                data-action="{{ route('restaurant.destroy',0) }}" >
+                <form id="deletePost" action="{{ route('restaurant.destroy', $restaurant->id) }}"
+                    data-action="{{ route('restaurant.destroy', $restaurant->id) }}" method="POST">
                     @method('DELETE')
                     @csrf
-                    <button type="submit" onclick="eliminar" class="btn btn-primary">Confirmar</button>
+                    <button type="submit" class="btn btn-primary">Save changes</button>
                 </form>
             </div>
         </div>
     </div>
-</div> --}}
+</div>
 
 <script>
     window.onload = function() {
+
         $('#exampleModal').on('show.bs.modal', function(event) {
             // Button that triggered the modal
             var button = $(event.relatedTarget)
             var id = button.data('id')
-            action = $('#delete').attr('data-action').slice(0,-1)
+            action = $('#deletePost').attr('data-action').slice(0, -1)
             action += id
             console.log(action)
-            $('#delete').attr('action', action)
+            $('#deletePost').attr('action', action)
             var modal = $(this)
-            modal.find('.modal-title').text('Vas a eliminar la publicación: ' + id)
+            modal.find('.modal-title').text('Vas a eliminar la categoría: ' + id)
+
         })
+
     }
 </script>
-
-
-
-  
-  <!-- Modal -->
-  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel"></h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                ¿Seguro deseas eliminar la publicación?
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
-                <form id="delete" action="{{ route('restaurant.destroy',0) }}" method="POST"
-                data-action="{{ route('restaurant.destroy',0) }}" >
-                    @method('DELETE')
-                    @csrf
-                    <button type="submit" class="btn btn-primary">Confirmar</button>
-                </form>
-            </div>
-        </div>
-    </div>
-  </div>
