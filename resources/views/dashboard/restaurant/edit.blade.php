abad81b62f4ce9456332c8a76e5f4699a5f39ecb
JuanJosePazUC <juan.1701712480@ucaldas.edu.co>
2021-12-12 19:06:13 -0500

    Show

--- resources/views/dashboard/restaurant/edit.blade.php
+++ resources/views/dashboard/restaurant/edit.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <br>
     <h4>Editar restaurante</h4><br>
-    <form action="{{ route('restaurant.update', $restaurant -> id) }}" method="POST">
+    <form action="{{ route('restaurant.update', $restaurant->id) }}" method="POST">
         @method('PUT')
         @include('dashboard.restaurant._form')</form>
 @endsection
